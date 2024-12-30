e00ced147198b82000be8489fe84bd5481380b86
Daniel Johnson <danielrjohnson00@gmail.com>
2024-12-30 17:18:56 -0600

    revert

--- update_database.py
+++ update_database.py
@@ -10,8 +10,7 @@ from match import Match
 
 def scrape_videos(channel_id: str, title_regex: str) -> list[dict]:
     """ Search a YouTube channel for all videos matching a regex and return them. """
-    opts = {"ignoreerrors": True, "matchtitle": title_regex,
-            "daterange": DateRange(start="today-1week")}
+    opts = {"ignoreerrors": True, "matchtitle": title_regex}
     with yt_dlp.YoutubeDL(opts) as ydl:
         uploads_playlist_id = channel_id.replace(
             "UC", "UU")  # UC is channel, UU is uploads
