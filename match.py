01235fb34f1c15d8af92881c3bfc06aa26f5e8f4
Daniel Johnson <39017265+DanielRJohnson@users.noreply.github.com>
2024-12-30 17:41:06 -0600

    Update DB, add new characters, fix a new description bug, and style (#10)
    * update character lists
    
    * add date restriction
    
    * fix yt_dlp DateRange usage
    
    * update db manually
    
    * fix description error
    
    * revert
    
    * trying sleep interval
    
    * remove sleep

--- match.py
+++ match.py
@@ -28,15 +28,22 @@ class Match:
         """ Given a chapter, return the names and characters for p1 and p2. """
         # helper functions
         strip_all = lambda strings: [s.strip() for s in strings]
-        remove_whitespace_and_lower_all = lambda strings: ["".join(s.split()).lower() for s in strings]
         split_player = lambda s: s.split(")")[0].split("(", maxsplit=1) if "(" in s and ")" in s else [s, "Not Provided"]
+        remove_whitespace_and_lower_all = lambda strings: ["".join(s.split()).lower() for s in strings]
         take_first_char = lambda s: re.split(r"[ \t,/&]+", s)[0]
 
         # split chapter title into names and characters, normalize to lower case with no whitespace, and take first character
         p1, p2 = strip_all(re.split(r"vs.|vs", chapter["title"], maxsplit=1))
-        p1_name, p1_char = remove_whitespace_and_lower_all(split_player(p1))
+
+        p1_split, p2_split = split_player(p1), split_player(p2)
+        if len(p1_split) == 1:  # edge-case
+            p1_split = [p1_split[0], "Not Provided"]
+        if len(p2_split) == 1:
+            p2_split = [p2_split[0], "Not Provided"]
+        
+        p1_name, p1_char = remove_whitespace_and_lower_all(p1_split)
         p1_char = take_first_char(p1_char)
-        p2_name, p2_char = remove_whitespace_and_lower_all(split_player(p2))
+        p2_name, p2_char = remove_whitespace_and_lower_all(p2_split)
         p2_char = take_first_char(p2_char)
 
         return p1_name, p1_char, p2_name, p2_char
--- update_database.py
+++ update_database.py
@@ -11,7 +11,8 @@ def scrape_videos(channel_id: str, title_regex: str) -> list[dict]:
     """ Search a YouTube channel for all videos matching a regex and return them. """
     opts = {"ignoreerrors": True, "matchtitle": title_regex}
     with yt_dlp.YoutubeDL(opts) as ydl:
-        uploads_playlist_id = channel_id.replace("UC", "UU") # UC is channel, UU is uploads
+        uploads_playlist_id = channel_id.replace(
+            "UC", "UU")  # UC is channel, UU is uploads
         playd = ydl.extract_info(uploads_playlist_id, download=False)
     return playd["entries"]
 
@@ -42,7 +43,7 @@ def insert_match_into_database(match: Match, conn: sqlite3.Connection) -> None:
         print(e)
         cur.execute("ROLLBACK TRANSACTION;")
 
-    
+
 def process_search(row: list, conn: sqlite3.Connection) -> None:
     """ For one search defined in search_list.csv find each video and update the database. """
     videos = scrape_videos(row["channel_id"], row["channel_search_regex"])
@@ -50,13 +51,17 @@ def process_search(row: list, conn: sqlite3.Connection) -> None:
 
 
 if __name__ == '__main__':
-    search_list = read_csv("search_list.csv") # list of channels to search, what to search for, etc.
+    # list of channels to search, what to search for, etc.
+    search_list = read_csv("search_list.csv")
 
     with sqlite3.connect("matches.db") as conn:
-        def process_search_partial(r): # multiprocessing pool likes only one argument
+        # multiprocessing pool likes only one argument
+        def process_search_partial(r):
             process_search(r, conn)
 
-        # for each search, update the database 
-        n_workers = min(len(search_list), len(os.sched_getaffinity(0))) # max procs needed
+        # for each search, update the database
+        n_workers = min(len(search_list), len(
+            os.sched_getaffinity(0)))  # max procs needed
         with Pool(n_workers) as pool:
-            pool.map(process_search_partial, [row for _, row in search_list.iterrows()])
+            pool.map(process_search_partial, [
+                     row for _, row in search_list.iterrows()])
