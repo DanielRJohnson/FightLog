6a080d2db49872b0f9d0c17de6886db8e6e72cb5
Daniel <danielrjohnson00@gmail.com>
2025-01-25 20:59:47 +0000

    add quiet option to YoutubeDL

--- update_database.py
+++ update_database.py
@@ -9,7 +9,7 @@ from match import Match
 
 def scrape_videos(channel_id: str, title_regex: str) -> list[dict]:
     """ Search a YouTube channel for all videos matching a regex and return them. """
-    opts = {"ignoreerrors": True, "matchtitle": title_regex}
+    opts = {"ignoreerrors": True, "matchtitle": title_regex, "quiet": True}
     with yt_dlp.YoutubeDL(opts) as ydl:
         uploads_playlist_id = channel_id.replace(
             "UC", "UU")  # UC is channel, UU is uploads
